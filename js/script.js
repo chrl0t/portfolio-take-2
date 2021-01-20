c02695067c225ab0afcd48a350e6145634b97b67
chrl0t <charlotte_i@live.co.uk>
2021-01-20 16:10:35 +0000

    nav bar

--- js/script.js
+++ js/script.js
@@ -20,4 +20,20 @@ $(document).ready(function () {
     filter: "*",
     animationOptions: { duration: 1500, easing: "linear", queue: false }
   });
+
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+    const body = $("body");
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
 });

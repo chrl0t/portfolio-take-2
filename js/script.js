bde88f2246deb06a8f9117aaab5741c82760b681
chrl0t <charlotte_i@live.co.uk>
2021-01-20 15:09:27 +0000

    worked on portfolio section

--- js/script.js
+++ js/script.js
@@ -13,4 +13,11 @@ $(document).ready(function () {
     startDelay: 1000,
     showCursor: false
   });
+
+  $("[data-fancybox]").fancybox;
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: { duration: 1500, easing: "linear", queue: false }
+  });
 });
